eb0ddf44f520c0468b141eb9bffa58ff8debff6a
Lucas Sousa Meireles <meireles31@msn.com>
2016-03-29 13:35:13 +0100

    Implemented adduser

--- public/javascripts/poesales.js
+++ public/javascripts/poesales.js
@@ -3,7 +3,52 @@
  */
 
 $(document).ready(function(){
-    $('#regButton').click( function(){
-        alert('oi')
-    })
+    $('#regButton').on('click', addUser)
 })
+
+function addUser(event){
+  event.preventDefault();
+  var newEntry = {
+    'email' : $('input#regemail').val()
+  }
+
+  $.ajax({
+    type: 'POST',
+    data: newEntry,
+    url: '/emails/addemail',
+    dataType: 'JSON'
+  }).done(function(response){
+    if (response.msg === ''){
+      alert('Data Added Successfuly!')
+    }else{
+      alert('Error' + response.msg);
+    }
+
+  })
+}
+
+// function deleteUser(event){
+//     event.preventDefault();
+//
+//     var confirmation = confirm('Are you sure you wanto to delete this user?');
+//
+//     if (confirmation === true){
+//         var email = {
+//             'email' : $('input #regemail').val()
+//         }
+//         $.ajax({
+//             type:'DELETE',
+//             data : email,
+//             url : '/emails/deleteemail/' + $(this).attr('')
+//         }).done(function(response){
+//             if (response.msg === ''){
+//                 alert('Email sucessfully removed!')
+//             }else{
+//                 alert('Error: ' + response.msg);
+//             }
+//             //populateTable();
+//         })
+//     }else{
+//         return false;
+//     }
+// };
--- routes/emails.js
+++ routes/emails.js
@@ -13,4 +13,30 @@ router.get('/', function(req, res){
     });
 });
 
+router.post('/addemail', function(req, res){
+  var db = req.db;
+
+  var email = req.body.email;
+
+  var collection = db.get('emails');
+
+  collection.insert({
+    "email" : email
+  }, function(err){
+    res.send( ( err == null)? {msg: ''}: { msg: 'error' + err})
+  })
+})
+
+router.delete('/deleteemail/:id', function(req, res){
+    var db = req.db;
+    var collection = db.get('emails');
+    var emailID = req.params.id
+    collection.remove(
+        //{'_id':emailToDelete},
+        req.body,
+        function(err){
+        res.send( ( err == null)? {msg: ''}: { msg: 'error' + err})
+    })
+})
+
 module.exports = router;
