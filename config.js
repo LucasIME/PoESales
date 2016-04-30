87ac0bf81abccf9adc9aa12dc25f48a79d4f4e89
Lucas Sousa Meireles <meireles31@msn.com>
2016-04-30 12:44:59 +0100

    Added config.js template

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    dbusername : "yourMLabUsername",
+    dbpassword : "yourMlabPassword",
+    sourceEmail : "emailsender@bot.com",
+    sourceEmailPassword : "senderemailpassword"
+}
