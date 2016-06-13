b816b221675af828d0d360d0517697b784a5c6bf
Lucas Sousa Meireles <meireles31@msn.com>
2016-06-13 09:03:08 +0100

    Changed base URL

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
     sourceEmail : "lucasimemailbot@gmail.com",
     sourceEmailPassword : "IMPA2013go!",
     sendgridAPI : "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",
-    baseURL : "poesales-app.appspot.com"
+    baseURL : "poesales.herokuapp.com"
 }
