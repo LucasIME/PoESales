ff083b0a8a37d3fc5d26244f5493489620184bb3
Lucas Meireles <meireles31@msn.com>
2016-10-27 11:11:16 -0200

    removed config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    dbusername : "lucasime",
-    dbpassword : "IMPA2013go!",
-    sourceEmail : "lucasimemailbot@gmail.com",
-    sourceEmailPassword : "IMPA2013go!",
-    sendgridAPI : "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",
-    baseURL : "poesales.herokuapp.com"
-}
