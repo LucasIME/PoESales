38b4812530dcc84f0efb5ee02c7f0ef3eaeb4a96
Lucas Meireles <meireles31@msn.com>
2017-09-07 17:16:58 +0000

    Added router to project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
 import './App.css';
-import Header from './Header/Header'
-import Body from './Body/Body'
-import Footer from './Footer/Footer'
+import Header from './Header/Header';
+import Body from './Body/Body';
+import Footer from './Footer/Footer';
+import Unregister from './Unregister/Unregister';
+import { BrowserRouter as Router,  Route } from 'react-router-dom';
 
 class App extends React.Component { render() {
     return (
-    <div>
-      <Header/>
-      <Body />
-      <Footer/>
-    </div>
-    )
+    <Router>
+      <div>
+        <Header/>
+        <Route exact={true} path="/" component={Body} />
+        <Route path="/unregister" component={Unregister} />
+        <Footer/>
+      </div>
+    </Router>
+    );
   }
 }
 
