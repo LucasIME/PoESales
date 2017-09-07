98be364bf3a94dc70836f0b03ec63cc8fe3310a6
Lucas Meireles <meireles31@msn.com>
2017-09-07 20:46:03 +0000

    added proxy to react and changed configs on server

--- app.js
+++ app.js
@@ -30,22 +30,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/react', function(req, res){
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.use(function(req, res, next){
   req.db = db;
   next();
 });
 
-app.use('/', routes);
 app.use('/users', users);
 app.use('/emails', emails);
 
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
