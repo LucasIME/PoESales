00dd2d5ae6a3422aef59965f00a718a5e818d992
Lucas Meireles <meireles31@msn.com>
2017-12-26 17:55:09 -0200

    Refactored sendmails and renamed it to sendmails.js

---
+++ bin/sendmails.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+//custom imports
+var request = require("request");
+var cheerio = require("cheerio");
+var sourceEmail = process.env.sourceEmail;
+var baseURL = process.env.baseURL;
+if (process.env.NODE_ENV === "dev") {
+  var baseURL = "localhost:" + String(process.env.PORT);
+}
+
+var PoEMail = require("../helpers/mailhelper.js");
+var sendgridAPI = process.env.sendgridAPI;
+var mailHelper = new PoEMail(sendgridAPI, sourceEmail);
+
+var monk = require("monk");
+
+var dbusername = process.env.dbusername;
+var dbpassword = process.env.dbpassword;
+var db = monk(
+  "mongodb://" +
+    dbusername +
+    ":" +
+    dbpassword +
+    "@ds011168.mlab.com:11168/poesales"
+);
+
+var collection = db.get("emails");
+var url = "https://www.pathofexile.com/shop/category/daily-deals";
+
+request(url, function(error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html);
+    //console.log(html);
+    var itemPricesDic = {};
+    //create itemprice dictionary based on items in discount
+    $(".shopItemBase").each(function() {
+      var itemName = $(this)
+        .children()
+        .first()
+        .next()
+        .text();
+      var itemPrice = $(this)
+        .children()
+        .first()
+        .next()
+        .next()
+        .text();
+      itemPricesDic[itemName] = itemPrice;
+    });
+
+    //sends email to each email in the database
+    collection
+      .find({})
+      .then(function(itemsVec) {
+        console.log(itemsVec);
+        for (var index in itemsVec) {
+          var targetEmail = itemsVec[index].email;
+          mailHelper.sendPromoMail(targetEmail, itemPricesDic, function(err, json) {
+            if (err) {
+              console.log("Error sending message to: " + targetEmail, err);
+            } else {
+              console.log(json);
+              console.log("Message sent to : " + targetEmail);
+            }
+          });
+        }
+      })
+      .catch(function(err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+  }
+});
